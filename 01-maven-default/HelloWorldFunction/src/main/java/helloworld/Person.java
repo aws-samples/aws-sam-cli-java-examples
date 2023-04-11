b238b735a21f8b1d15bd009b0487e8bb4d79531d
Maximilian Schellhorn <mxschell@amazon.de>
2023-04-11 12:28:38 +0200

    Adapted sample code

--- 01-maven-default/HelloWorldFunction/src/main/java/helloworld/Person.java
+++ 01-maven-default/HelloWorldFunction/src/main/java/helloworld/Person.java
@@ -1,14 +1,15 @@
 package helloworld;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 class Person implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String name;
 
-    public String getName() {
-        return name;
+    public Optional<String> getName() {
+        return Optional.ofNullable(name);
     }
 
     public void setName(String name) {
